c44d92448c63b7ab8e1d3bad86bdcc13444a8470
antonio martinez baez <tono.mtzb@gmail.com>
2020-09-18 14:43:54 -0500

    completed robot sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -67,11 +67,11 @@ class SortingRobot:
         If the held item's value is equal, return 0.
         If either item is None, return None.
         """
-        if self._item is None or self._list[self._position +1] is None:
+        if self._item is None or self._list[self._position] is None:
             return None
-        elif self._item > self._list[self._position + 1 ]:
+        elif self._item > self._list[self._position]:
             return 1
-        elif self._item < self._list[self._position + 1]:
+        elif self._item < self._list[self._position]:
             return -1
         else:
             return 0
@@ -92,20 +92,45 @@ class SortingRobot:
         """
         return self._light == "ON"
 
+    def pick_up(self):
+        """
+        Picks up a item. 
+        If the robot is already holding an item, it swaps instead
+        """
+        if self._item is None:
+            self._item = self._list[self._position]
+            self._list.pop(self._position)
+        else:
+            self.swap_item()
+
+
     def sort(self):
         """
         Sort the robot's list.
         """
+
         self.set_light_on()
 
         while self._light == 'ON':
+            self._position = 0
             self.set_light_off()
-            if self.can_move_right():
-                self._item = self._list[self._position]
+            while self.can_move_right():
+                self.swap_item()
                 self.move_right()
-                if self.compare_item() == 1:
+                # held item is greater
+                if self.compare_item() == 1 :
                     self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
                     self.set_light_on()
+                if self.compare_item() == -1 or self.compare_item() == 0:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                
+
+                   
 
         return self._list
 
@@ -116,7 +141,7 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-    l = [9, 4 , 2, 4, 6]
+    l = [9, 6 ,4, 2, 4]
     robot = SortingRobot(l)
 
     robot.sort()
