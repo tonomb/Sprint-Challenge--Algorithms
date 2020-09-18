c0ec3385e15b5e0ed85ac8f71d7c061b6ec815b4
antonio martinez baez <tono.mtzb@gmail.com>
2020-09-18 12:52:07 -0500

    working on robot

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -67,11 +67,11 @@ class SortingRobot:
         If the held item's value is equal, return 0.
         If either item is None, return None.
         """
-        if self._item is None or self._list[self._position] is None:
+        if self._item is None or self._list[self._position +1] is None:
             return None
-        elif self._item > self._list[self._position]:
+        elif self._item > self._list[self._position + 1 ]:
             return 1
-        elif self._item < self._list[self._position]:
+        elif self._item < self._list[self._position + 1]:
             return -1
         else:
             return 0
@@ -96,16 +96,27 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        self.set_light_on()
+
+        while self._light == 'ON':
+            self.set_light_off()
+            if self.can_move_right():
+                self._item = self._list[self._position]
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+
+        return self._list
+
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [9, 4 , 2, 4, 6]
     robot = SortingRobot(l)
 
     robot.sort()
